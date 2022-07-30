66b777e873de5826e3c3f661b2554f5f38411694
depombo <lfdepombo@gmail.com>
2022-07-30 13:25:26 -0700

    reset hrefs on route change

--- telemetry.js
+++ telemetry.js
@@ -3,6 +3,18 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 const amplitudeKey = '1f380d1286396641d1e9a56f15e80e65';
 
+function addDeviceId() {
+  // pass device id
+  const deviceId = amplitude.getInstance().options.deviceId;
+  // TODO change back to prod dashboard
+  document.querySelectorAll("[href='https://app.iasql.com']")
+    .forEach(n => n.href = `https://app-staging.iasql.com?amp_device_id=${deviceId}`);
+  document.querySelectorAll("[href='https://blog.iasql.com']")
+    .forEach(n => n.href = `https://blog.iasql.com?amp_device_id=${deviceId}`);
+  document.querySelectorAll("[href='https://iasql.com']")
+    .forEach(n => n.href = `https://iasql.com?amp_device_id=${deviceId}`);
+}
+
 export function onRouteDidUpdate({location, previousLocation}) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
@@ -10,25 +22,15 @@ export function onRouteDidUpdate({location, previousLocation}) {
     amplitude.getInstance().logEvent("DOCS", {
       route: location.pathname,
     });
+    addDeviceId();
   }
 }
 
 // browser only
 if (ExecutionEnvironment.canUseDOM) {
   // https://developers.amplitude.com/docs/advanced-settings#cross-domain-tracking-javascript
-  amplitude.getInstance().init(amplitudeKey, null, {includeReferrer: true, includeUtm: true, deviceIdFromUrlParam: true});
+  amplitude.getInstance().init(amplitudeKey, null, {includeReferrer: true, includeUtm: true, deviceIdFromUrlParam: true}, addDeviceId);
   amplitude.getInstance().logEvent("DOCS", {
     route: document.location.pathname,
   });
-  window.onload = function() {
-    // pass device id
-    const deviceId = amplitude.getInstance().options.deviceId;
-    // TODO change back to prod dashboard
-    document.querySelectorAll("[href='https://app.iasql.com']")
-      .forEach(n => n.href = `https://app-staging.iasql.com?amp_device_id=${deviceId}`);
-    document.querySelectorAll("[href='https://blog.iasql.com']")
-      .forEach(n => n.href = `https://blog.iasql.com?amp_device_id=${deviceId}`);
-    document.querySelectorAll("[href='https://iasql.com']")
-      .forEach(n => n.href = `https://iasql.com?amp_device_id=${deviceId}`);
-  }
 }
