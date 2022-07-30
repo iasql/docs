748411417b0fd62c7740b2a68319c5e510beffc3
depombo <lfdepombo@gmail.com>
2022-07-30 12:04:45 -0700

    simplify logic

--- telemetry.js
+++ telemetry.js
@@ -1,19 +1,16 @@
 import amplitude from 'amplitude-js';
 
-const amplitudeKey = "1f380d1286396641d1e9a56f15e80e65";
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-// grab userId given by dashboard via url params if available
-amplitude.getInstance().init(amplitudeKey, params.ampUserId, {includeReferrer: true, includeUtm: true});
+// https://developers.amplitude.com/docs/advanced-settings#cross-domain-tracking-javascript
+amplitude.getInstance().init(amplitudeKey, params.ampUserId, {includeReferrer: true, includeUtm: true, deviceIdFromUrlParam: true});
 amplitude.getInstance().logEvent("DOCS", {
   route: document.location.pathname,
 });
 const deviceId = amplitude.getInstance().options.deviceId;
 window.onload = function() {
-  // pass device id to dashboard
-  // TODO remove app-staging and use app until e2e testing
-  document.querySelector("[href='https://app.iasql.com']").href = `https://app-staging.iasql.com?ampDeviceId=${deviceId}`
+  // pass device id
+  document.querySelectorAll("[href='https://app.iasql.com']").map(n => n.href = `https://app-staging.iasql.com?amp_device_id=${deviceId}`);
+  document.querySelectorAll("[href='https://blog.iasql.com']").map(n => n.href = `https://blog.iasql.com?amp_device_id=${deviceId}`);
+  document.querySelectorAll("[href='https://iasql.com']").map(n => n.href = `https://iasql.com?amp_device_id=${deviceId}`);
 }
 // Listen on and log changes to href
 // https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
